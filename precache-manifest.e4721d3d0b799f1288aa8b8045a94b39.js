0522a0f2262f81bcf389e1559c86bda0b7fb3c11
Nazar <somewhereseen@gmail.com>
2020-01-28 10:30:14 +0200

    Updates

---
+++ precache-manifest.e4721d3d0b799f1288aa8b8045a94b39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a1d961ba27f437ecc9273e8c750f4f8",
+    "url": "/react-social-network/index.html"
+  },
+  {
+    "revision": "7f1b520d4aab08043168",
+    "url": "/react-social-network/static/css/main.d20b97f3.chunk.css"
+  },
+  {
+    "revision": "9af609ab6ae9e7fa8af9",
+    "url": "/react-social-network/static/js/2.84c03b9b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-social-network/static/js/2.84c03b9b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f1b520d4aab08043168",
+    "url": "/react-social-network/static/js/main.d8474204.chunk.js"
+  },
+  {
+    "revision": "db3a3e285fa1d1331625",
+    "url": "/react-social-network/static/js/runtime-main.674caaec.js"
+  }
+]);
