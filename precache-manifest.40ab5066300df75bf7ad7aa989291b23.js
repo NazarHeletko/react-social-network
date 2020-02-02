78dc278aca3ee4962167e40745ce04244bc1b9e3
Nazar <somewhereseen@gmail.com>
2020-02-02 21:46:30 +0200

    Updates

--- precache-manifest.40ab5066300df75bf7ad7aa989291b23.js
+++ precache-manifest.40ab5066300df75bf7ad7aa989291b23.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1f5f9245c0cd890136796c7ad1fdbfdf",
+    "revision": "9581a77139200b9df2183c07763276a2",
     "url": "/react-social-network/index.html"
   },
   {
-    "revision": "a5e87f6a8a142e24a739",
-    "url": "/react-social-network/static/css/main.0090f729.chunk.css"
+    "revision": "f2163e68fed9fa41cd43",
+    "url": "/react-social-network/static/css/main.0edc9daf.chunk.css"
   },
   {
-    "revision": "95a70174c6a5a42914be",
-    "url": "/react-social-network/static/js/2.cfbfe5a8.chunk.js"
+    "revision": "d5fc6897417db4fb80dd",
+    "url": "/react-social-network/static/js/2.97584f9c.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/react-social-network/static/js/2.cfbfe5a8.chunk.js.LICENSE"
+    "url": "/react-social-network/static/js/2.97584f9c.chunk.js.LICENSE"
   },
   {
-    "revision": "a5e87f6a8a142e24a739",
-    "url": "/react-social-network/static/js/main.2c302cd6.chunk.js"
+    "revision": "f2163e68fed9fa41cd43",
+    "url": "/react-social-network/static/js/main.e9304320.chunk.js"
   },
   {
     "revision": "db3a3e285fa1d1331625",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-social-network/precache-manifest.35f4c7582cd4e4aae537de64c97184a7.js"
+  "/react-social-network/precache-manifest.40ab5066300df75bf7ad7aa989291b23.js"
 );
 
 self.addEventListener('message', (event) => {
